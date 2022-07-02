19227976595a20c20ef2f1128ec6de2a86515e63
clinonaminu66 <ologbonyohamidu@gmail.com>
2022-07-02 15:02:10 -0700

    more commit

---
+++ activityseven.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+
+void main() {
+  String flower = 'rose';
+  var tree = 'apple';
+  print('before swapping');
+  print('flower = rose, tree = apple');
+  
+  print('flower = $tree, tree = $flower');
+  
+}
---
+++ activitysix.dart
@@ -0,0 +1,9 @@
+void main() {
+  userProfile('Hamidu', 2002, 10000.00, true, 'computer scientist');
+  
+  }
+  void userProfile(String name, int age, double salary, bool isMale, String occupation){
+  
+    print('His name is $name and he is ${2022-2000} years old. He is a software developer that earns \$$salary weekly');
+
+  }
